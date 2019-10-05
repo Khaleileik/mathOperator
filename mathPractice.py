985bf5bab81ebf6366cabe259c4f734e97659fb6
Khaleilei Kidau <Khaleileik@users.noreply.github.com>
2019-10-05 11:25:35 -0500

    Initial commit

---
+++ mathPractice.py
@@ -0,0 +1,47 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+a = 17
+
+b = 32
+
+c = a + b
+
+print(c)
+
+x = 29
+
+y = 69
+
+z = x * y
+
+print(z)
+
+f = x + c
+
+print(f)
+
+g = f * z
+
+print(g)
+
+print(a-c)
+
+print(g-f)
+
+print(f/g)
+
+print(a/z)
+
+print(f//a)
+
+print(y//g)
+
+print(b%f)
+
+print(x%b)
+
+
+
